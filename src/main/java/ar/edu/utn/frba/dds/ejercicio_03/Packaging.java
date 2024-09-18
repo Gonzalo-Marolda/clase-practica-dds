c371c1815a855c967f136132b849dffa06be70b4
Gonzalo Agustin Marolda <gmarolda@frba.utn.edu.ar>
2024-09-18 19:53:25 -0300

    wawwawawa

---
+++ src/main/java/ar/edu/utn/frba/dds/ejercicio_03/Packaging.java
@@ -0,0 +1,32 @@
+package ar.edu.utn.frba.dds.ejercicio_03;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table(name = "decorado")
+public class Packaging extends Decorado{
+
+  @Column(name = "precio")
+  private Double precio;
+
+  //* ----------------- CONSTRUCTORS ----------------- *//
+
+  public Packaging() {
+  }
+
+  public Packaging(Double precio) {
+      this.precio = precio;
+  }
+
+  //* ----------------- METHODS ----------------- *//
+
+  public Double precio() {
+    return this.producto.precio() + this.precio;
+  }
+
+}
